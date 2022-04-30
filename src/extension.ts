0fd26cba24c88d82927b25275d06c7cda29fde2e
DANCHE <dangdancheg@gmail.com>
2022-04-30 18:12:01 +0300

    added another command

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,26 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "grpc-rocket" is now active!');
-  let disposable = vscode.commands.registerCommand(
-    "grpc-rocket.helloWorld",
-    () => {
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("grpc-rocket.helloWorld", () => {
       vscode.window.showInformationMessage("Hola bruh!");
-    }
+    })
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("grpc-rocket.askQuestion", async () => {
+      let answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+      console.log(answer);
+      if (answer === "good") {
+        vscode.window.showInformationMessage("Gut");
+      }
+    })
+  );
 }
 
 export function deactivate() {}
